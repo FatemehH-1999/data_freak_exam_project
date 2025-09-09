a28cfc9d1d24bae14ec1a6e72e2ce6db790bc6bc
Fatemeh Sadat Heidari Rahmatabadi <fahei3407@uib.no>
2025-09-09 21:37:35 +0200

    Day7

--- scripts/Day6-F.R
+++ scripts/Day6-F.R
@@ -19,5 +19,35 @@ data2 <-
   drop_na(multi_ct_result_orderset)
 data2
 
+#arrange ID
+data2 <-
+  data2 %>%
+  arrange(ID)
+data2
+
+#A column showing whether rec_ver_tat is higher than 100 or not: values High/Low
+data2 <- data2 %>% 
+  distinct() %>%
+  pivot_wider(
+    names_from = time_measurement,
+    values_from = value)
+  
+data2 <- data2 %>%
+  mutate(rec_ver_tat_level = if_else(rec_ver_tat <= 100, "Low", "High"))
+data2
+
 #Read and join the additional dataset to your main dataset
+
+library(tidyverse)
+library(here)
+exam_data_join <- read_delim(here("data", "exam_data_join.txt"), delim = "\t")
+
+data2 <- left_join(data2, exam_data_join, join_by( c("ID" == "id" )))
+data2
+
+data2 <- left_join(data2, exam_data_join, by = c("ID" = "id"))
+data2
+
+
+
 
