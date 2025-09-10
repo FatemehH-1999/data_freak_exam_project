d34f354f23c6492df9ca1c620b8dd0761ac63ae4
Pratik Khanal <iampratikkhanal@gmail.com>
2025-09-10 15:09:51 +0200

    commit

--- scripts/Day6_Pratik.R
+++ scripts/Day6_Pratik.R
@@ -17,10 +17,35 @@ pivot_wider(
 
 exam_data_tidy_wide <- exam_data_tidy_wide %>%
   mutate(rec_ver_tat_level = if_else(rec_ver_tat <= 100, "Low", "High"))
-
-merged_df <- left_join(
+#merging two datasets 
+exam_data_merged <- left_join(
   exam_data_tidy_wide,
   exam_data_join,
   by = c("ID" = "id")
 )
+View(exam_data_merged)
+#developing .txt file for merged data
+fileName <- paste0("exam_data_merged", ".txt")
+write_delim(
+  exam_data_merged, 
+  file = here("data", fileName),
+  delim = "\t"
+)
+merged_data <- read_delim(here("data", "exam_data_merged.txt"))
+view(merged_data)
+#Stratify your data by a categorical column and report min, max, mean and sd of a numeric column
+#Only for persons with patient_class == inpatient
+merged_data %>%
+  filter(patient_class == "inpatient") %>% 
+  group_by(gender) %>%
+  summarise(
+    min_value  = min(pan_day,na.rm = TRUE),
+    max_value  = max(pan_day,na.rm = TRUE),
+    mean_value = mean(pan_day,na.rm = TRUE),
+    sd_value   = sd(pan_day,na.rm = TRUE),
+    .groups = "drop"
+)
+#Use two categorical columns in your dataset to create a table (either with table(), count(), or janitor::tabyl())
+merged_data %>%
+  with(table(gender, rec_ver_tat_level))
 
