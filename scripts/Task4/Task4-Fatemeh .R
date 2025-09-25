d089d37740de49fc7d2d0a272082918db311731b
Fatemeh Sadat Heidari Rahmatabadi <fahei3407@uib.no>
2025-09-25 20:50:13 +0200

    changes

--- scripts/Task4/Task4-Fatemeh .R
+++ scripts/Task4/Task4-Fatemeh .R
@@ -8,6 +8,7 @@ data <- read_delim(here("data", "exam_data_tidier_2025-09-09.txt"), delim = " ")
 data
 
 # Boxplot: compares medians & spread
+##The boxplot shows that the distribution of ct_result is very similar for females and males, with no obvious difference between the two sex group
 ggplot(data, aes(x = gender, y = ct_result, fill = gender)) +
   geom_boxplot(alpha = 0.7, outlier.color = "red") +
   labs(title = "Distribution of ct_result by gender",
@@ -17,6 +18,7 @@ ggplot(data, aes(x = gender, y = ct_result, fill = gender)) +
   theme(legend.position = "none")
 
 # Violin plot: shows full distribution shape
+##The violin plot indicates that the ct_result distributions for females and males are nearly identical, showing no meaningful difference between the two sex groups
 ggplot(data, aes(x = gender, y = ct_result, fill = gender)) +
   geom_violin(trim = FALSE, alpha = 0.6) +
   geom_boxplot(width = 0.1, fill = "white") +
@@ -27,6 +29,7 @@ ggplot(data, aes(x = gender, y = ct_result, fill = gender)) +
   theme(legend.position = "none")
 
 # Density plot: both groups overlaid
+##The density plot shows that the ct_result distributions for females and males almost completely overlap
 ggplot(data, aes(x = ct_result, colour = gender, fill = gender)) +
   geom_density(alpha = 0.4) +
   labs(title = "Density of ct_result by gender",
